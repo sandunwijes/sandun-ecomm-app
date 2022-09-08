2718d7c899939c27125c0b0d2beb7750b5645c17
SANDUN WIJESINGHE <sandunnw@gmail.com>
2022-09-08 21:43:20 +0530

    Add files via upload

---
+++ client/build/precache-manifest.7c5a2998da27003018deffa7e6633a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e04e226ff307de0dc5d",
+    "url": "/static/css/main.d7018e64.chunk.css"
+  },
+  {
+    "revision": "4e04e226ff307de0dc5d",
+    "url": "/static/js/main.7a8b0890.chunk.js"
+  },
+  {
+    "revision": "88f4af2b835b3d9c05c6",
+    "url": "/static/js/runtime~main.f8b3415f.js"
+  },
+  {
+    "revision": "e6a7d419c7c7489e6d43",
+    "url": "/static/css/2.d1d79f5b.chunk.css"
+  },
+  {
+    "revision": "e6a7d419c7c7489e6d43",
+    "url": "/static/js/2.51f2bd54.chunk.js"
+  },
+  {
+    "revision": "82ca8046d9914395d4da",
+    "url": "/static/js/3.b192dfdb.chunk.js"
+  },
+  {
+    "revision": "5e8ae94b3b4a7a3101c8adda315610c8",
+    "url": "/index.html"
+  }
+];
