a3ee597b7f703c247714a8360823264db2febb99
sandunwijes <107408557+sandunwijes@users.noreply.github.com>
2022-09-09 10:01:32 +0530

    make it better

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/orders/', orderRoute);
     app.use('/',express.static('client/build'));
 
     app.get('*',(req,res)=>{
-      res.send(path.resolve(__dirname, 'client/build/index.html'));
+      res.send(path.resolve(__dirname, 'client','build','index.html'));
     })
 
   }
